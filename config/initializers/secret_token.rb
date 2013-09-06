356a6d023a6badc517c4391929bb8ad28aefd344
Kordyuk Jura <jura@webkate.com>
2013-09-06 18:42:46 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRTest::Application.config.secret_token = '1c38d36b51c02571d80b09dac470eeac6d2667e1efed0c89803d1319db2fa0b2259abf3325aacaa051ec8c7cf3b24ca2b1c660be6482e559ef89dd8f419bcf4e'
